9a41c31e01b15e433310c1d9e1132cbb4d041916
Arjit <arjitmalviya@gmail.com>
2016-06-30 22:13:02 +0530

    added more test cases

--- src/test/java/com/cinemalytics/sdk/java/MovieRepositoryTest.java
+++ src/test/java/com/cinemalytics/sdk/java/MovieRepositoryTest.java
@@ -3,6 +3,8 @@
  */
 package com.cinemalytics.sdk.java;
 
+import java.util.List;
+
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
@@ -17,13 +19,29 @@ public class MovieRepositoryTest extends TestCase {
 
 	private final String testMovieId = "3b8b23f0";
 	
-	private final String testToken = "A97AB577D11D30C2EDF8274F16CD5B80";
+	private final String testToken = "A97AB577D11D30C2EDF8274F16CD5B80"; // do not use this token in production, your app will break!
 	
+	Cinemalytics cm = new Cinemalytics(testToken);
+	
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	public void testGetMovieById() throws Exception {
-		
-		Cinemalytics cm = new Cinemalytics(testToken);
+				
 		Movie movie = cm.GetMovieRepositoryInstance().GetMovieById(testMovieId);		
 		Assert.assertNotNull(movie);
 		Assert.assertEquals(movie.getId(), testMovieId);
+	}
+	
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	public void testGetReleasedThisWeekMovies() throws Exception {
+				
+		List<Movie> movies = cm.GetMovieRepositoryInstance().GetReleasedThisWeekMovies();		
+		Assert.assertNotNull(movies);
+		assertFalse(movies.size() == 0);		
 	}
 }
